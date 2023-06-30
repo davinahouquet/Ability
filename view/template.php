f898f548067497e2ceef372f277c78c37089fcb6
davinahouquet <davina.houquet@hotmail.fr>
2023-06-30 16:02:52 +0200

    Ajout des vues Connexion et Login

--- view/template.php
+++ view/template.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="http://localhost/Ability/public/css/style.css">
     <title>Ability</title>
 </head>
@@ -12,37 +13,37 @@
 <header>
     <nav>
         <div class="ability-logo">
-            <img src="#">
+            <i class="fa-solid fa-hands"></i>
             <p class="ability">Ability</p>
         </div>
         <ul>
-            <p class=""><a href="index.php?action=#">Catégories</a></p>
+            <p><a href="index.php?action=#" class="navigation-item">Catégories</a></p>
 
             <!-- CONNEXION - COMPTE - UTILISATEURS - ADMIN -->
             <?php
                 // if NOT connected...
             ?>
-            <p class=""><a href="index.php?action=#">Connexion</a></p>
+            <p ><a href="index.php?action=#" class="navigation-item">Connexion</a></p>
 
             <?php
                 //else if connected...
             ?>
-            <p class=""><a href="index.php?action=#">$account-name</a></p>
+            <!-- <p class=""><a href="index.php?action=#">$account-name</a></p> -->
 
             <?php
                 //else if username...
             ?>
-            <p class=""><a href="index.php?action=#">$username</a></p>
+            <!-- <p class=""><a href="index.php?action=#">$username</a></p> -->
 
             <?php
                 //else if Admin...
             ?>
-            <p class=""><a href="index.php?action=#">Admin</a></p>
+            <!-- <p class=""><a href="index.php?action=#">Admin</a></p> -->
 
             <?php
                 //else ...
             ?>
-            <p class=""><a href="index.php?action=#">Super Admin</a></p>
+            <!-- <p class=""><a href="index.php?action=#">Super Admin</a></p> -->
 
         </ul>
     </nav>
